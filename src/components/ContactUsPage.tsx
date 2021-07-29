2edf7572772780f9e33c6e35f0f23bfba348ff06
ibrahim çarbuğa <carbugaibrahim@hotmail.com>
2021-07-29 21:00:42 +0300

    Component structures continuing

--- src/components/ContactUsPage.tsx
+++ src/components/ContactUsPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 
 export const ContactUsPage = () => {
@@ -12,31 +12,76 @@ export const ContactUsPage = () => {
     { id: "BR", name: "Brazil" },
     { id: "ZW", name: "Zimbabwe" },
   ];
+  const [user, setUser] = useState({
+    userName: "",
+    email: "",
+    phone: "",
+    textField: "",
+    country: "",
+  });
+
+  const submit = (e: any) => {
+    e.preventDefault();
+    alert(JSON.stringify({ user }));
+    // fetch("/api", {
+    //   method: "POST",
+    //   body: JSON.stringify({ user }),
+    //   headers: { "Content-Type": "application/json" },
+    // })
+    //   .then((res) => res.json())
+    //   .then((json) => setUser(json.user));
+  };
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={submit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            name="userName"
+            value={user.userName}
+            onChange={(e) => setUser({ ...user, userName: e.target.value })}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="number" placeholder="Phone Number" />
+          <Form.Control
+            type="number"
+            placeholder="Phone Number"
+            name="phone"
+            value={user.phone}
+            onChange={(e) => setUser({ ...user, phone: e.target.value })}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+        <Form.Group className="mb-3" controlId="textField">
+          <Form.Label>Text Field</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="textField"
+            value={user.textField}
+            onChange={(e) => setUser({ ...user, textField: e.target.value })}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="countryDropdown">
           <Form.Label>Country</Form.Label>
-          <Form.Control as="select">
+          <Form.Control as="select" value={user.country} onChange={(e) => setUser({ ...user, country: e.target.value })}>
             {countryList.map((country) => (
-              <option value={country.name} key={country.id}>
+              <option
+                value={user.country}
+                key={country.id}>
                 {country.name}
               </option>
             ))}
--- src/components/LocaleDropdown.tsx
+++ src/components/LocaleDropdown.tsx
@@ -2,6 +2,21 @@ import React from "react";
 import { Dropdown } from "react-bootstrap";
 
 export const LocaleDropdown = () => {
+  var languages = [
+    { id: 1, value: "tr" },
+    { id: 2, value: "en" },
+  ];
+  const [value, setValue] = React.useState("");
+  
+ 
+  React.useEffect(() => {
+    localStorage.setItem("language", value);
+  }, [value]);
+
+  const onSelect = (value: string) => {
+    setValue(value);
+  };
+
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -9,8 +24,14 @@ export const LocaleDropdown = () => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">TR</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">EN</Dropdown.Item>
+        {languages.map((language) => (
+          <Dropdown.Item
+            key={language.id}
+            onSelect={() => onSelect(language.value)}
+          >
+            {language.value}
+          </Dropdown.Item>
+        ))}
       </Dropdown.Menu>
     </Dropdown>
   );
